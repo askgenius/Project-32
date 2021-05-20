6989cdb0d65c034583a81ea25b21d5eb7960950d
askgenius <askhara84@gmail.com>
2021-05-20 20:38:32 +0530

    fourth commit

--- sketch.js
+++ sketch.js
@@ -8,6 +8,7 @@ var backgroundImg;
 
 var bg;
 
+
 function preload() {
     // create getBackgroundImg( ) here
     getBackgroundImg();
@@ -21,7 +22,7 @@ function setup() {
 
 function draw() {
 
-    if(backgroundImg)
+    if (backgroundImg)
         background(backgroundImg);
     // add condition to check if any background image is there to add
 
@@ -29,8 +30,9 @@ function draw() {
     Engine.update(engine);
 
     // write code to display time in correct format here
+
     textSize(35)
-    text("Time: "+  hour , width - 300, 50)
+    text("Time: " + hour + ":00 hrs" , width - 700, 50)
 
 }
 
@@ -40,36 +42,36 @@ async function getBackgroundImg() {
     var response = await fetch("http://worldtimeapi.org/api/timezone/Asia/Kolkata");
     //change the data in JSON format
     var responseJSON = await response.json()
-    var datetime = responseJSON.utc_datetime
+    var datetime = responseJSON.datetime
     // write code slice the datetime
-    var hour = datetime.slice(11, 13);
-    console.log("hour : "+ hour);
+    hour = datetime.slice(11, 13);
+    console.log("hour : " + hour);
 
     // add conditions to change the background images from sunrise to sunset
     if (hour >= 04 && hour <= 06) {
         bg = "sunrise1.png";
     } else if (hour >= 06 && hour <= 08) {
         bg = "sunrise2.png";
-    } else if (hour >= 06 && hour <= 08) {
+    } else if (hour >= 08 && hour <= 10) {
         bg = "sunrise3.png";
-    } else if (hour >= 06 && hour <= 08) {
+    } else if (hour >= 10 && hour <= 12) {
         bg = "sunrise4.png";
-    } else if (hour >= 06 && hour <= 08) {
+    } else if (hour >= 12 && hour <= 14) {
         bg = "sunrise5.png";
-    } else if (hour >= 06 && hour <= 08) {
+    } else if (hour >= 14 && hour <= 16) {
         bg = "sunrise6.png";
-    } else if (hour >= 06 && hour <= 08) {
-        bg = "sunrise7.png";
-    } else if (hour >= 06 && hour <= 08) {
-        bg = "sunrise8.png";
-    } else if (hour >= 06 && hour <= 08) {
-        bg = "sunrise9.png";
-    } else if (hour >= 23 && hour <= 0) {
-        bg = "sunrise10.png";
-    } else if (hour == 0 && hour <= 03) {
-        bg = "sunrise11.png";
-    } else if (hour >= 06 && hour <= 08) {
-        bg = "sunrise12.png";
+    } else if (hour >= 16 && hour <= 18) {
+        bg = "sunset7.png";
+    } else if (hour >= 18 && hour <= 20) {
+        bg = "sunset8.png";
+    } else if (hour >= 20 && hour <= 22) {
+        bg = "sunset9.png";
+    } else if (hour >= 22 && hour <= 24) {
+        bg = "sunset10.png";
+    } else if (hour => 00 && hour <= 02) {
+        bg = "sunset11.png";
+    } else if (hour >= 02 && hour <= 04) {
+        bg = "sunset12.png";
     }
 
     //load the image in backgroundImg variable here
